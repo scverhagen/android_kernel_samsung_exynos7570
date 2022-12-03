a54cdc693cb5499055e387ca886b963c0e18e560
Steve Verhagen <scverhagen@gmail.com>
2022-12-03 07:22:46 +0000

    update build_kernel.sh script

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7570-on5xelte_defconfig
+make exynos7570-gtesveltevzw_defconfig
 make -j$(nproc --all)
